65e4a94dd5596e5651b063ac9cc619b90ffe01d8
hilde03 <78738649+hilde03@users.noreply.github.com>
2022-05-20 10:24:05 +0200

    d

--- oppgave_3/kode.js
+++ oppgave_3/kode.js
@@ -5,10 +5,10 @@ let nvmeBtn = document.getElementById("nvmeBtn");
 let infoDiv = document.getElementById("infoDiv");
 
 hddBtn.addEventListener("click", function() {
-    infoDiv.innerHTML = "";
-    let hddDiv = document.createElement("div");
-    infoDiv.appendChild(hddDiv);
-    hddDiv.innerHTML = "Harddisk: " + "500 GB";
+    infoDiv.innerHTML = "lorem ipsum";
+    let hddImg = document.createElement("img");
+    hddImg.src = "bilder/hdd.png";
+    infoDiv.appendChild(hddImg);
 });
 
 ssdBtn.addEventListener("click", function() {
@@ -19,3 +19,11 @@ ssdBtn.addEventListener("click", function() {
 
 });
 
+nvmeBtn.addEventListener("click", function() {
+    infoDiv.innerHTML = "lorem ipsum";
+    let nvmeImg = document.createElement("img");
+    nvmeImg.src = "bilder/nvme.png";
+    infoDiv.appendChild(nvmeImg);
+});
+
+// https://moonbooks.org/Articles/How-to-add-an-image-in-a-HTML-page-using-javascript-/ (for å legge inn bildene)
