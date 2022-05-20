458f4f771f9ad7757023e06a79b55889188a6924
hilde03 <78738649+hilde03@users.noreply.github.com>
2022-05-20 13:38:58 +0200

    h

--- oppgave_3/kode.js
+++ oppgave_3/kode.js
@@ -5,21 +5,24 @@ let nvmeDiv = document.getElementById("nvmeDiv");
 let infoDiv = document.getElementById("infoDiv");
 
 hddDiv.addEventListener("click", function() {
-    infoDiv.innerHTML = "lorem ipsum";
+    infoDiv.innerHTML = "Platelager (engelsk harddisk, forkortet HD, eller hard disk drive, HDD) er et lagringsmedium for binært kodet informasjon på hurtig roterende platelagerskiver.";
+    // https://no.wikipedia.org/wiki/Platelager
     let hddImg = document.createElement("img");
     hddImg.src = "bilder/hdd.png";
     infoDiv.appendChild(hddImg);
 });
 
 ssdDiv.addEventListener("click", function() {
-    infoDiv.innerHTML = "lorem ipsum";
+    infoDiv.innerHTML = 'SSD (Solid state drive) er et lagringsmedium som benytter flashminne istedenfor mekanisk/magnetiske plater til å lagre data.';
+    // https://no.wikipedia.org/wiki/Solid_state_drive
     let ssdImg = document.createElement("img");
     ssdImg.src = "bilder/ssd.png";
     infoDiv.appendChild(ssdImg);
 });
 
 nvmeDiv.addEventListener("click", function() {
-    infoDiv.innerHTML = "lorem ipsum";
+    infoDiv.innerHTML = "NVMe (nonvolatile memory express) er en ny lagringstilgang og transportprotokoll for flash og neste generasjons solid-state-stasjoner (SSD-er) som leverer den høyeste gjennomstrømningen og raskeste responstidene ennå for alle typer bedriftsarbeidsbelastninger.";
+    // https://www.netapp.com/data-storage/nvme/what-is-nvme/
     let nvmeImg = document.createElement("img");
     nvmeImg.src = "bilder/nvme.png";
     infoDiv.appendChild(nvmeImg);
@@ -33,3 +36,7 @@ hddDiv.addEventListener("click", changeText);
 function changeText() {
     div.innerHTML = "lorem ipsum";
 };
+
+// har dønn brukt over en time på å prøve å få koden til å funke uten at jeg skjønte hvorfor den ikke gjorde, så så jeg at jeg hadde glemt å linke til js:(
+// så kvaliteten på denne oppgaven blir ikke helt optimal akkurat
+// blir så oppgitt:/
