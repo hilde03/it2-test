f5c401b8fc24a44dd26205dc523b40019c62df79
hilde03 <78738649+hilde03@users.noreply.github.com>
2022-05-20 11:47:26 +0200

    f

--- oppgave_2/kode.js
+++ oppgave_2/kode.js
@@ -1,13 +1,6 @@
 let divs = document.getElementsByTagName("div");
 
 
-// function changeSizeAndColor() {
-//     divs.style.color = "blue";
-    
-// };
-
-// changeSizeAndColor();
-
 let color = [, "#3C9EE7", "#E7993C", "#E73C99", "#3CE746", "#E7993C"];
   
 document.querySelector("div").addEventListener("click", function () {
@@ -16,3 +9,15 @@ document.querySelector("div").addEventListener("click", function () {
         });
 
 // https://www.geeksforgeeks.org/how-to-change-background-color-of-a-div-on-mouse-move-over-using-javascript/
+
+//  funksjon som endrer farge og størrelse på divs
+
+function changeColor() {
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].style.background = "blue";
+        divs[i].style.width = "150px";
+        divs[i].style.height = "150px";
+    }
+}
+
+changeColor();
