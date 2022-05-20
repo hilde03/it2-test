b424ea315d7ec5ccd4d2d9b9a56ca367380d6f62
hilde03 <78738649+hilde03@users.noreply.github.com>
2022-05-20 12:14:15 +0200

    n

--- oppgave_3/kode.js
+++ oppgave_3/kode.js
@@ -1,17 +1,17 @@
-let hddBtn = document.getElementById("hddBtn");
-let ssdBtn = document.getElementById("ssdBtn");
-let nvmeBtn = document.getElementById("nvmeBtn");
+let hddDiv = document.getElementById("hddDiv");
+let ssdDiv = document.getElementById("ssdDiv");
+let nvmeDiv = document.getElementById("nvmeDiv");
 
 let infoDiv = document.getElementById("infoDiv");
 
-hddBtn.addEventListener("click", function() {
+hddDiv.addEventListener("click", function() {
     infoDiv.innerHTML = "lorem ipsum";
     let hddImg = document.createElement("img");
     hddImg.src = "bilder/hdd.png";
     infoDiv.appendChild(hddImg);
 });
 
-ssdBtn.addEventListener("click", function() {
+ssdDiv.addEventListener("click", function() {
     infoDiv.innerHTML = "lorem ipsum";
     let ssdImg = document.createElement("img");
     ssdImg.src = "bilder/ssd.png";
@@ -19,7 +19,7 @@ ssdBtn.addEventListener("click", function() {
 
 });
 
-nvmeBtn.addEventListener("click", function() {
+nvmeDiv.addEventListener("click", function() {
     infoDiv.innerHTML = "lorem ipsum";
     let nvmeImg = document.createElement("img");
     nvmeImg.src = "bilder/nvme.png";
